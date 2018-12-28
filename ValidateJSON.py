e4a0ee5b56485ae44bc3e97c10a96f397839e72f
abdouseck632 <35151324+abdouseck632@users.noreply.github.com>
2018-12-28 04:03:17 +0000

    Update ValidateJSON.py

--- ValidateJSON.py
+++ ValidateJSON.py
@@ -1,22 +1,12 @@
 #il faudra passser en argument un fichier JSON
-import sys
-
+import sys	
 import json
-from jsonschema import validate
-
-# fonction de validation du ficheir json
-
-def fonction_demo(dict_to_test, dict_valid):
+def json_validator(data):
     try:
-        validate(dict_to_test, dict_valid)
-    except Exception as valid_err:
-        print("Validation NO: {}".format(valid_err))
-        raise valid_err
-    else:
-        # Realise votre travail
-        print("JSON validé")
-fichier_json = sys.argv[1]
+        json.loads(data)
+        return True
+    except ValueError as error:
+        print("invalid json: %s" % error)
+        return False
 if __name__ == '__main__':
-#permet de verifier la validation avec la fonction fonction_demo    
-    with open(fichier_json, "r") as fichier:
-        dict_to_test = json.load(fichier)        
+	print (json_validator(sys.argv[1]))
